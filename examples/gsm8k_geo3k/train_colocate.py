6516236e7a3ec84ea5baf58fd170511d538b4f81
Sun Jiaxuan <jiaxuan040827@gmail.com>
2025-12-26 00:29:02 +0800

    Restore default GPU settings

--- examples/gsm8k_geo3k/train_colocate.py
+++ examples/gsm8k_geo3k/train_colocate.py
@@ -184,7 +184,7 @@ def train(args):
         )
 
         if args.fsdp:
-            initial_model = strategy.prepare_model(initial_model, is_training=False, shard_size=2)
+            initial_model = strategy.prepare_model(initial_model, is_training=False, shard_size=8)
             strategy.offload_model(initial_model)
 
     if args.enable_ema:
